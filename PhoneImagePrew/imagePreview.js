20ebf49ebaff134d2852c7f1f8f016b40ca0484e
chenshuangyu <695166116@qq.com>
2016-05-03 23:15:51 +0800

    宽度自适应 和原图查看

--- PhoneImagePrew/imagePreview.js
+++ PhoneImagePrew/imagePreview.js
@@ -35,7 +35,7 @@
         $("body").append('\
             <div id="imageModal" style="display:none;position:fixed;opacity:1;background-color:black;top:0px;left:0px;right:0px;bottom:0px;">\
                 <div id="imageHead" style="border-bottom: 1px solid white;height:20px;z-index:9999;background-color:black;position:absolute;top:0;left:0;right:0;"></div>\
-                <div id="imageBody" style="position:relative;overflow: scroll;height:'+(window.innerHeight-42)+'px;"></div>\
+                <div id="imageBody" style="position:relative;overflow: scroll;height:' + (window.innerHeight - 42) + 'px;margin-top: 21px;"></div>\
                 <div id="imageFooter" style="border-top:1px solid white;height:20px;position:fixed;bottom:0px;left:0px;right:0px;background-color:black;"></div>\
             </div>');
 
@@ -74,16 +74,35 @@
 
         $("#imageModal>#imageBody img[imgguid]").each(function(){
             $(this).click(function () {//.dblclick(function () {
-                var winW = window.innerWidth;
+                var winW = $(this).closest("#imageBody").width();
+                var winH = $(this).closest("#imageBody").height();
                 var imgW = $(this).width();
            
-                console.log("winW:" + winW);
-                console.log("imgW:" + imgW);
-                if (winW==imgW) {
-                    $(this).css("width", "auto");
+              //  console.log("winW:" + winW);
+               // console.log("imgW:" + imgW);
+                if (winW == imgW) { //放大
+                    var tranX =tranY= "-50%";
+                    $(this).css({ "width": "auto" });
+
+                    if (winW < $(this).width()) { //如果图片的真实宽度大于窗口的宽度
+                        $(this).css({ left: "0" });
+                        tranX = "0";
+                    }
+                    if (winH < $(this).height()) { //如果图片的真实高度大于窗口的高度
+                        $(this).css({ top: "0" });
+                        tranY = "0";
+                    }
+                    $(this).css("transform", "translate("+tranX+","+tranY+")");
+
+                    var scrollL = ($(this).width() - winW) / 2;
+                    var scrollT = ($(this).height() - winH) / 2;
+                    if(scrollL>0)
+                        $(this).parent().scrollLeft(scrollL);
+                    if(scrollT>0)
+                        $(this).parent().scrollTop(scrollT);
                 }
-                else {
-                    $(this).width(winW);
+                else {   //缩小
+                    $(this).width(winW).css({ position: "absolute",left:"50%",top:"50%", "transform": "translate(-50%,-50%)" });
                 }
             });
         });
